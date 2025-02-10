7ad2094cea8474a873eb19be96a8f0428ab159b6
vickychaudhary <vickyofficial1399@gmail.com>
2025-02-10 17:16:57 +0530

    Test5

--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import react from '@vitejs/plugin-react-swc';
-import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
-import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
-import log from 'snore-log';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const envVariables = loadEnv(mode, process.cwd());
-  return {
-    plugins: [react(), splitVendorChunkPlugin(), log()],
-    publicDir: 'public',
-    define: {
-      // Inject environment variables into the client-side code
-      'process.env': {
-        VITE_DCL_DEFAULT_ENV: envVariables.VITE_DCL_DEFAULT_ENV,
-        VITE_BASE_URL: envVariables.VITE_BASE_URL,
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js')],
+    }),
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
       },
-      global: {},
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
-    resolve: {
-      alias: {
-        util: 'rollup-plugin-node-polyfills/polyfills/util',
-        assert: 'rollup-plugin-node-polyfills/polyfills/assert',
-        assets: '/src/assets',
-        components: '/src/components',
-        containers: '/src/containers',
-        contracts: '/src/contracts',
-        helpers: '/src/helpers',
-        hooks: '/src/hooks',
-        providers: '/src/providers',
-      },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
     },
-    server: {
-      open: false, // Automatically open the browser
-      proxy: {},
-      hmr: {
-        overlay: false, // Disable HMR overlay (not recommended for production)
-      },
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        loader: {
-          '.js': 'jsx', // Allow parsing of JSX syntax in .js files
-        },
-        define: {
-          global: 'globalThis', // Change Node.js global to browser globalThis
-        },
-        plugins: [
-          NodeGlobalsPolyfillPlugin({ buffer: true, process: false }),
-          NodeModulesPolyfillPlugin(),
-        ],
-      },
-    },
-    build: {
-      commonjsOptions: {
-        transformMixedEsModules: true, // Allow CommonJS modules to be used with ES modules
-      },
-      rollupOptions: {
-        plugins: [rollupNodePolyFill()], // Polyfills for Node.js built-ins
-      },
-      sourcemap: false, // Enable sourcemaps for debugging
-    },
-    ...(command === 'build' ? { base: envVariables.VITE_BASE_URL } : undefined), // Set base URL for production
-  };
+  },
 });
