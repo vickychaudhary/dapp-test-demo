bbda211a97732dbf766401e78ed3b2ec0d17bc7a
vickychaudhary <vickyofficial1399@gmail.com>
2025-02-10 17:22:03 +0530

    Configure Vite path aliases for easier module imports

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,12 @@ export default defineConfig({
       include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js')],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      src: path.resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       define: {
